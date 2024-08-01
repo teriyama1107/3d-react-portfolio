e254fcd469fcad655ab6d947cb17797370312539
Ryo <r-tabata@lockup.jp>
2024-08-01 23:26:30 +0900

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,11 @@
+
+import { motion } from 'framer-motion';
+import { ComputersCanvas } from './canvas';
+
+const Hero = () => {
+  return (
+    <div>Hero</div>
+  )
+};
+
+export default Hero;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,71 @@
+import React, {useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
+import { styles } from '../styles';
+import { navLinks } from '../constants';
+import { logo, menu, close } from '../assets';
+
+const Navbar = () => {
+  // コンポーネントの中で使ってる状態管理
+  const [active, setActive] = useState('');
+  const [toggle, setToggle] = useState(false);
+
+  return (
+    <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
+      <div className="w-full flex justify-between max-w-7xl mx-auto">
+        <Link
+          to="/"
+          className="flex items-center gap-2"
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}
+        >
+        <img src={logo} alt="logo" className="w-9 h-9 object-contain"/>
+        <p className="text-white text-[18px] font-bold cursor-pointer">Ryo Tabata <span className="sm:block hidden">| JavaScript Mastery</span></p>
+        </Link>
+        <ul className="list-none hidden sm:flex flex-row gap-10">
+          {navLinks.map((link) => (
+            <li key={link.id}
+                className={`${active === link.title ? "text-white" : "text-secondary"} hover:text-white text-[18px] font-medium cursor-pointer`}
+                onClick={() => {
+                  setActive(link.title);
+                  // そして、activeにリンクのタイトルをセットしてるよー。
+                  // つまり、リンクをクリックしたときに、そのリンクのタイトルがactive状態になる！
+                }}
+            >
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
+        <div className="sm:hidden flex flex-1 justify-end items-center">
+          <img 
+            src={toggle ? close : menu}
+            alt="menu"
+            className="w-[28] h-[28px] object-contain cursor-pointer"
+            onClick={() => setToggle(!toggle)}
+            // ここでtoggleの状態をひっくり返してる。今がfalseならtrueに、trueならfalseになる
+            // だから、スイッチがONからOFF、OFFからONって切り替わる！
+          />
+        </div>
+        <div className={`${!toggle ? 'hidden': 'flex'} p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w[140px] z-10 rounded-xl`}>
+        <ul className="list-none flex justyfy-end items-start flex-col gap-4">
+          {navLinks.map((link) => (
+            <li key={link.id}
+                className={`${active === link.title ? "text-white" : "text-secondary"} font-poppins font-medium cursol-pointer text-[16px]`}
+                onClick={() => {
+                  setToggle(!toggle); 
+                  setActive(link.title);
+                }}
+            >
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
+        </ul>
+        </div>
+      </div>
+    </nav>
+  )
+};
+
+export default Navbar;

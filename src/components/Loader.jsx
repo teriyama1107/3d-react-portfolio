bda226feebdc925cfe5def4b51a0dd9ed00777de
Ryo <r-tabata@lockup.jp>
2024-08-08 09:31:10 +0900

    heroセクションの途中

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -3,29 +3,17 @@ import { Html, useProgress } from "@react-three/drei";
 const CanvasLoader = () => {
   const { progress } = useProgress();
   return (
-    <Html
-      as='div'
-      center
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        flexDirection: "column",
-      }}
-    >
-      <span className='canvas-loader'></span>
+    <Html>
+      <span className="canvas-load"></span>
       <p
-        style={{
-          fontSize: 14,
-          color: "#F1F1F1",
-          fontWeight: 800,
-          marginTop: 40,
-        }}
-      >
-        {progress.toFixed(2)}%
-      </p>
+      style= {{
+        fontSize: "14",
+        color: "#f1f1f1",
+        marginTop: "40"
+      }}
+      >{progress.toFixed(2)}%</p>
     </Html>
-  );
+  )
 };
 
 export default CanvasLoader;
--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -5,27 +5,27 @@ import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 
 const Computers = ({ isMobile }) => {
-  const computer = useGLTF("./desktop_pc/scene.gltf");
+  const computer = useGLTF("./desktop_pc/heri/scene.gltf");
 
   
 
   return (
     <mesh>
       {/* intensityは光の強さを制御 primitiveコンポーネントはcomputerをsceneに追加しています。*/}
-      <hemisphereLight intensity={0.15} groundColor="black" /> 
-      <pointLight intensity={1}/>
+      <hemisphereLight intensity={3} groundColor="black" /> 
+      <pointLight intensity={4}/>
       <spotLight 
-        position={[-20, 50, 10]}
+        position={[-20, 30, 10]}
         angle={0.12}
         penumbra={1}
-        intensity={1}
+        intensity={10000}
         castShadow
         shadow-mapSize={1024}
       />
       <primitive 
         object={computer.scene}
         scale={0.75}
-        position={[0, -3.25, -1.5]}
+        position={[-1, -2.25, -0.5]}
         rotation={[-0.01, -0.2, -0.1]}
       />
     </mesh>
@@ -33,6 +33,26 @@ const Computers = ({ isMobile }) => {
 }
 
 const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(max-width: 500px)');
+    setIsMobile(mediaQuery.matches);
+
+    setIsMobile(mediaQuery.matches);
+
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    }
+
+    mediaQuery.addEventListener('change', handleMediaQueryChange);
+
+    return () => {
+      mediaQuery.removeEventListener('change', handleMediaQueryChange);
+    }
+
+  }, []);
+
   return (
     <Canvas
       frameloop = "demand" 
@@ -49,7 +69,7 @@ const ComputersCanvas = () => {
           maxPolarAngle={Math.PI / 2 }
           minPolarAngle={Math.PI / 2 }
         />
-      <Computers />
+      <Computers isMobile={isMobile} />
       </Suspense>
       {/*
       enableZoomはzoom機能を無効にする。マウスホイールやタッチ操作ができなくなる。

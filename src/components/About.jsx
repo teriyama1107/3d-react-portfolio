16eac73c16f2214dd2f0e3f8c7fa3245d36c1c95
Ryo <r-tabata@lockup.jp>
2024-08-22 09:28:31 +0900

    About セクションの途中

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,8 +1,27 @@
 import React from "react";
+//import Tilt from "react-tilt";
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+//import { service } from "../constants";
+import { fadeIn, textVariant } from '../utils/motion';
+
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+    <motion.p>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[20px] max-w-3xl leading-[30px]"
+      />
+      I'ma wew develper
+      </motion.p>
+    </>
   )
 };
 
